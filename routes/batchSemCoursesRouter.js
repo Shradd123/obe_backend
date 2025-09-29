0052149d743d460632c46f7247ad6872c017a97e
JnaneshwariRaoB <151983410+JnaneshwariRaoB@users.noreply.github.com>
2025-09-29 17:35:26 +0530

    batchsemcourse sep 29

--- routes/batchSemCoursesRouter.js
+++ routes/batchSemCoursesRouter.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { getCoursesByBatchSem } = require('../controllers/batchSemCoursesController');
+const { getCoursesByBatchSems } = require('../controllers/batchSemCoursesController');
 
 // GET /api/batch/:batchId/sem/:semId/courses
 router.get('/batch/:batchId/sem/:semId/courses', getCoursesByBatchSem);
+router.get('/batch/:batchId/sems/:semId/courses', getCoursesByBatchSems);
 
 module.exports = router;
