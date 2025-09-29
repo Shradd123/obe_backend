04520174c25685c15f8b17bdf7184ea6df42fda0
Shradd123 <srdshetty27@gmail.com>
2025-09-29 17:05:20 +0530

    changes on 29/9/25

--- routes/coPoPsoMappingRoutes.js
+++ routes/coPoPsoMappingRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const coPoPsoController = require('../controllers/coPoPsoController');
+const coPoPsoController = require('../controllers/coPoPsoMappingController');
 
 // Get COs for a course
 router.get('/course-outcome/:courseId', coPoPsoController.getCourseOutcomes);
